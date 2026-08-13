1953cd127780d4890b2ea378520b0f78dba75ea1
jun394647 <jun394647@gmail.com>
2026-08-13 15:46:07 +0900

    제출 기준에 맞춰 MODE를 exercise로 변경

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import FarmApp from './FarmApp.vue'
 
 // 'practice' = 수업 예제 화면, 'exercise' = 과제 제출용 화면, 'farm' = 농사 날씨 커스텀 화면
 // 과제 제출 시에는 반드시 'exercise'로 되돌려 놓을 것
-const MODE = 'farm'
+const MODE = 'exercise'
 </script>
 
 <template>
